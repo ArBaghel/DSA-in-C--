828542fa679f824e0379122bf691bc9762949def
ArBaghel <163445825+ArBaghel@users.noreply.github.com>
2026-01-20 14:10:22 +0530

    Pattern in cpp

---
+++ Lecture 3- 20 Jan 2026/cb10.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=1;j--){
+            if (j<=n-i+1){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+           
+             
+        }
+        for (int j=i;j<=n-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+        
+    }
+}
---
+++ Lecture 3- 20 Jan 2026/cb9.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if (j<=n-i ){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+           
+             
+        }
+        for (int k=2;k<=i;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+        
+        
+        
+    }
+} 
