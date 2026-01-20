d80886bdc5bafa20b1da7d3d0133e09a1990ad85
ArBaghel <163445825+ArBaghel@users.noreply.github.com>
2026-01-20 22:44:48 +0530

    lecture 3 pattern 15

--- Lecture 3- 20 Jan 2026/cb15.cpp
+++ Lecture 3- 20 Jan 2026/cb15.cpp
@@ -1,5 +1,45 @@
-# include <iostream>
+// *  *  *  *  * 
+//        *  *  *  *
+//              *  *  *
+//                    *  *
+//                          *
+//                    *  *
+//              *  *  *
+//        *  *  *  *
+//  *  *  *  *  *
+
+#include<iostream>
 using namespace std;
-int pattern(int n){
-    for (int )
+int main(){
+    int n=5;
+    for(int i=1;i<=n-1;i++){
+        for(int j=n;j>=1;j--){
+            if (j<=n-i+1){
+                cout<<" * ";
+            }
+            else{
+                cout<<"      ";
+            }
+           
+             
+        }
+        cout<<endl;
+        
+        
+    }
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if (j<=n-i){
+                cout<<"      ";
+            }
+            else{
+                cout<<" * ";
+            }
+           
+             
+        }
+        cout<<endl;
+        
+        
+    }
 }
