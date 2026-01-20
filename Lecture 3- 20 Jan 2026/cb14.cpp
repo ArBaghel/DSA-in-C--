ae64203d9f7393a5c13007ba729bfe2218385d3b
ArBaghel <163445825+ArBaghel@users.noreply.github.com>
2026-01-20 14:44:05 +0530

    Pattern in cpp

---
+++ Lecture 3- 20 Jan 2026/cb14.cpp
@@ -0,0 +1,31 @@
+# include <iostream>
+using namespace std;
+int pattern(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if (j<=n-i){
+                cout<<"   ";
+            }
+            else{
+                cout<<" * ";
+            }
+           
+             
+        }
+        cout<<endl;
+    }
+    for(int i = 1 ; i <= n-1 ; i++){
+        for(int j = 1 ; j <= i ; j++){
+            cout<<"   ";
+        }
+        for(int j = 1 ; j <= n - i ; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n=5;
+    cout<< pattern(n);
+
+}
