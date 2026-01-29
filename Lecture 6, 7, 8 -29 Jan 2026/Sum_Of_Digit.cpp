543bae93cb2d14b3b9dad607fa28a71bad936ea9
ArBaghel <163445825+ArBaghel@users.noreply.github.com>
2026-01-29 14:30:11 +0530

    Sum of the digits of a number

---
+++ Lecture 6, 7, 8 -29 Jan 2026/Sum_Of_Digit.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+int Sumdigit(int n){
+    int temp=n;
+    int sum=0;
+    while(temp>0){
+        int digit=temp%10;
+        sum+=digit;
+        temp/=10;
+    }
+    return sum;
+
+}
+int main(){
+    int n;
+    cout<<"Enter the Number:";
+    cin>>n;
+    cout<<"Sum of the digits of the number:"<<Sumdigit(n);
+    return 0;
+}
